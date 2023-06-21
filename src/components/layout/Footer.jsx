731192604621bb44ea7e5e6461a7563a75362468
Thiago Scholl <tcscholl@inf.ufpel.edu.br>
2023-06-21 15:45:34 -0300

    Update: Atualizando o Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,24 +4,23 @@ import logo from '../../assets/logoP.png';
 
 function Footer() {
     return (
-        <footer className="footer p-4 bg-default text-white">
-            <div className="container max-auto">
-                <img className="w-14 ml-2" src={logo} alt="Logo" />
-            </div>
-            <div className="container mt-2 -ml-36">
-                    <div className="flex footer-center space-x-10">
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Faça uma Doação
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Sobre Nós
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Contato
-                        </Link>
-                        
+        <footer className="footer p-3 bg-default text-white justify-center">
+            <div className="container mt-2">
+                <div className="flex footer-center space-x-20">
+                    <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
+                        Faça uma Doação
+                    </Link>
+                    <div className="container max-auto">
+                        <img className="w-14" src={logo} alt="Logo" />
                     </div>
+                    <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
+                        Contato
+                    </Link>
+                        
                 </div>
+            </div>
+
+
         </footer>
     )
 }
