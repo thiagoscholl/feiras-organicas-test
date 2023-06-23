3688ad96f17aca5337113d1d45344602f8a243e7
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-06-22 21:23:29 -0300

    config prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import MapboxMap from "../components/map/MapboxMap"
+import MapboxMap from "../components/map/MapboxMap";
 
 function Home() {
-    return (
-        <div className="flex">
-            <div className="w-2/5">
-                <h3 className="text-white">Esquerda</h3>Esquerda
-            </div>
-            <div className="w-3/5">
-
-                <MapboxMap />
-            </div>
-        </div>
-    )
+  return (
+    <div className='flex'>
+      <div className='w-2/5'>
+        <h3 className='text-white'>Esquerda</h3>Esquerda
+      </div>
+      <div className='w-3/5'>
+        <MapboxMap />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
