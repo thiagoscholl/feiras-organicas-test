b307842042f61df396e25014f0cea03eb50acc41
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-06-23 15:35:16 -0300

    consertando responsividade da navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,43 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import logo from '../../assets/logoG.png';
+import logo from "../../assets/logoG.png";
 
 function Navbar() {
-    return (
-        <nav className='navbar shadow-lg bg-default text-white'>
-            <div className="container max-auto">
-                <div className="flex-none px-2 mx-2">
-                    <Link to='/' className='text-lg align-middle' >
-                        <img
-                                className="w-48 ml-2"
-                                src={logo}
-                                alt="Logo"
-                            />
-                    </Link>
-                </div>
+  return (
+    <nav className='navbar shadow-lg bg-default text-white'>
+      <div className='container max-w-full'>
+        <div className='flex-none px-2 mx-2'>
+          <Link to='/' className='text-lg align-middle'>
+            <img
+              className='lg:w-48 ml-2 md:w-32 sm:w-24'
+              src={logo}
+              alt='Logo'
+            />
+          </Link>
+        </div>
 
-                <div className="flex-1 px-2 mx-2">
-                    <div className="flex justify-end">
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Mapa
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Sobre Nós
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Notícias
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Cadastrar Local
-                        </Link>
-                        <Link to='/' className="btn btn-ghost btn-sm rounden-btn">
-                            Login
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounden-btn'>
+              Mapa
+            </Link>
+            <Link to='/' className='btn btn-ghost btn-sm rounden-btn'>
+              Sobre Nós
+            </Link>
+            <Link to='/' className='btn btn-ghost btn-sm rounden-btn'>
+              Notícias
+            </Link>
+            <Link to='/' className='btn btn-ghost btn-sm rounden-btn'>
+              Cadastrar Local
+            </Link>
+            <Link to='/' className='btn btn-ghost btn-sm rounden-btn'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
+
